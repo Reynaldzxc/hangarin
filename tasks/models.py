b5ae4dd29c9a7ffcdee3bf60a7783b088002e357
Reynaldzxc <202480142@psu.palawan.edu.ph>
2026-09-16 20:51:33 +0800

    Add Hangarin models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        abstract = True
+
+class Priority(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Task(BaseModel):
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+
+    status = models.CharField(max_length = 100, choices = [("Pending", "Pending"), ("In Progress", "In Progress"),("Completed", "Completed")], default = "Pending")
+
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    priority = models.ForeignKey(Priority, on_delete = models.CASCADE)
+
+class Note(BaseModel):
+     task = models.ForeignKey(Task, on_delete = models.CASCADE)
+
+     content = models.TextField()
+
+
+class Subtask(BaseModel):
+     parent_task = models.ForeignKey(Task, on_delete = models.CASCADE)
+
+     title = models.CharField(max_length = 200)
+
+     status = models.CharField(max_length = 50, choices = [("Pending", "Pending"), ("In Progress", "In Progress"),("Completed", "Completed")], default = "Pending")
