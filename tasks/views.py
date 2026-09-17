4118931420b5c1093af7345427db0f8b7b24836f
Reynaldzxc <202480142@psu.palawan.edu.ph>
2026-09-17 20:08:54 +0800

    Add task status counts to dashboard

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Task
 
 def dashboard(request):
-    return render(request, "tasks/dashboard.html")
+    total_tasks = Task.objects.count()
+    pending_tasks = Task.objects.filter(status = "Pending").count()
+    completed_tasks = Task.objects.filter(status = "Completed").count()
+    in_progress_tasks = Task.objects.filter(status="In Progress").count()
+
+    context = {
+        "total_tasks": total_tasks,
+        "pending_tasks": pending_tasks,
+        "completed_tasks": completed_tasks,
+        "in_progress_tasks": in_progress_tasks,
+    }
+
+    return render(request, "tasks/dashboard.html", context)
